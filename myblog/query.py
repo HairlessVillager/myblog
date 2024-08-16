b2b61685406f56167896eb382920c06e0a2b954d
HairlessVillager <hairlessvillager@qq.com>
2024-08-16 21:25:49 +0800

    postgres -> mongodb

--- myblog/query.py
+++ myblog/query.py
@@ -1,56 +1,52 @@
 from logging import getLogger
 from typing import Sequence
 
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import async_sessionmaker
-
 from .model import (
     Blog,
-    engine,
+    client,
+)
+from .util import (
+    blog2md,
+    md2html,
 )
 
 logger = getLogger("app")
 
+blog_collection = client["myblog"]["blog"]
+
 
 async def get_blog(id: int) -> Blog | None:
-    async_session = async_sessionmaker(engine, expire_on_commit=False)
-    async with async_session() as session:
-        blog = await session.get(Blog, id)
-        if not blog:
-            logger.warning(
-                f"get_blog({id=}): blog not found"
-            )  # TODO: remove this warning
-        return blog
+    blog = await blog_collection.find_one({"id": id})
+    if not blog:
+        logger.warning(f"get_blog({id=}): blog not found")  # TODO: remove this warning
+        return None
+    return Blog.from_dict(blog)
 
 
 async def get_blogs_sorted_by_id() -> Sequence[Blog]:
-    async_session = async_sessionmaker(engine, expire_on_commit=False)
-    async with async_session() as session:
-        stmt = select(Blog).order_by(Blog.id)
-        blogs = (await session.scalars(stmt)).all()
-        return blogs
+    cursor = blog_collection.find().sort("id")
+    blogs = await cursor.to_list(length=None)
+    return [Blog.from_dict(b) for b in blogs]
 
 
 async def get_pinned_blogs_sorted_by_id() -> Sequence[Blog]:
-    async_session = async_sessionmaker(engine, expire_on_commit=False)
-    async with async_session() as session:
-        stmt = select(Blog).where(Blog.pinned == True).order_by(Blog.id)
-        blogs = (await session.scalars(stmt)).all()
-        return blogs
+    cursor = blog_collection.find({"pinned": True}).sort("id")
+    blogs = await cursor.to_list(length=None)
+    return [Blog.from_dict(b) for b in blogs]
 
 
 async def update_blog(blog: Blog):
-    async_session = async_sessionmaker(engine, expire_on_commit=False)
-    async with async_session() as session:
-        blog.create_at = blog.create_at.replace(tzinfo=None)
-        blog.update_at = blog.update_at.replace(tzinfo=None)
-        await session.merge(blog)
-        await session.commit()
+    logger.debug(f"update_blog: {blog=}")
+    blog.create_at = blog.create_at.replace(tzinfo=None)
+    blog.update_at = blog.update_at.replace(tzinfo=None)
+    blog.html = md2html(await blog2md(blog))
+    await blog_collection.update_one({"id": blog.id}, {"$set": blog.into_dict()})
+    new_blog = await blog_collection.find_one({"id": blog.id})
+    logger.debug(f"update_blog: {new_blog=}")
 
 
 async def create_blog(blog: Blog) -> int:
-    async_session = async_sessionmaker(engine, expire_on_commit=False)
-    async with async_session() as session:
-        session.add(blog)
-        await session.commit()
-        return blog.id
+    total = await blog_collection.count_documents({})
+    blog.id = total + 1
+    await blog_collection.insert_one(blog.into_dict())
+    return blog.id
